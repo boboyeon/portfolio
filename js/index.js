7080de28f7be77d9b55dae778b0457cebdc7a64c
boboyeon <hajoce02@naver.com>
2024-06-21 15:30:04 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//$('section').hide();
+
+setTimeout(function () {
+  $('.intro1').fadeOut(1000);
+},2000);
+
+setTimeout(function () {
+  $('.intro2').fadeOut(1000);
+},5000);
+
+setTimeout(function () {
+  $('.intro3').fadeOut(1000);
+},8000);
